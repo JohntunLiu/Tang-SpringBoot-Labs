30a8952be168ea505ebdbb70a139910d5f1b41dd
LZT <1216279343@qq.com>
2022-07-12 11:52:57 +0800

    配置启动

---
+++ lab-springmvc-23-02/src/main/java/cn/iocoder/springboot/lab23/springmvc/Application.java
@@ -0,0 +1,19 @@
+package cn.iocoder.springboot.lab23.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author LZT
+ * @data 2022/7/12
+ */
+@SpringBootApplication
+@ServletComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
