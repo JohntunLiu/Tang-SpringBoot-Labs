ec92e96cd204283cb90602363181a33d06ef6650
liuzongtang <liu_ztang@hisuntech.com>
2022-08-03 10:52:24 +0800

    序列化 反序列化案例

--- lab-00-java-demo/src/tangTest/service/TestDemo.java
+++ lab-00-java-demo/src/tangTest/service/TestDemo.java
@@ -1,15 +1,52 @@
 package tangTest.service;
 
 
+import sun.misc.Unsafe;
 import tangTest.bo.Major;
 import tangTest.bo.Student;
 
+import java.io.*;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+
 public class TestDemo {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        demo01();
+    public static void main(String[] args) throws CloneNotSupportedException, IOException, ClassNotFoundException {
+        serialize();
+        deserialize();
     }
 
-    static void demo02() {
+    static void serialize() throws IOException {
+        Major math = new Major("math", 23434l);
+        Student student = new Student("jack", 12l, math);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(new File("student.txt").toPath()));
+        objectOutputStream.writeObject(student);
+        objectOutputStream.close();
+        System.out.println("序列化成功！已经生成student.txt文件");
+        System.out.println("==============================================");
+    }
+
+    static void deserialize() throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(new File("student.txt").toPath()));
+        Student o = (Student) objectInputStream.readObject();
+        objectInputStream.close();
+        System.out.println("deserialize result:");
+        System.out.println(o);
+    }
+
+        static void demo02() {
+        try {
+            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            Unsafe unsafe = (Unsafe) f.get(null);
+            Student student =  (Student)unsafe.allocateInstance(Student.class);
+            System.out.println(student.setName("rouse").toString());
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        }
 
     }
     /*
